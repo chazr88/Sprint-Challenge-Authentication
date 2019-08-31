957300a1adfac496c994a9549256545fb4b73e4b
Chaz Rowe <chazr_88@hotmail.com>
2019-08-30 23:25:45 -0400

    almost finished

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -22,7 +22,7 @@ router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
   Users.findBy({ username })
-    //.first()
+    .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
         console.log('user')
